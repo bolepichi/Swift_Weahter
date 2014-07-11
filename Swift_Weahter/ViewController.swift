6304459ca65363a30db32916f553ba6d2bee058d
Leslie Han <lesliehan9210@gmail.com>
2014-07-11 16:27:56 +0800

    Initial Commit

---
+++ Swift_Weahter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_Weahter
+//
+//  Created by Taagoo'iMac on 14-7-11.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
